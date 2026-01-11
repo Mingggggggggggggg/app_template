4169ab5f8b295fd02bbfa48b0f05e28b840b4cfb
Ming <98337759+Mingggggggggggggg@users.noreply.github.com>
2026-01-11 17:16:22 +0100

    Initial Commit

---
+++ lib/data/constants.dart
@@ -0,0 +1,13 @@
+﻿import 'package:flutter/material.dart';
+
+class MainTextStyle {
+  static const TextStyle titleText = TextStyle(
+    color: Colors.teal,
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const descriptionText = TextStyle(
+    fontSize: 15
+  );
+}
